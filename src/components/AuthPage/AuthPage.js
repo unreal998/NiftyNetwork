6ddda6182ffb202cbeb17a1d1d54f4834f3f018b
Foegit <foegit@gmail.com>
2018-07-06 12:04:50 +0300

    AuthPage

--- src/components/AuthPage/AuthPage.js
+++ src/components/AuthPage/AuthPage.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import './AuthPage.scss';
 
 export default class AuthPage extends Component {
   render() {
     console.log(this);
     return (
-      <div className="AuthPageContainer">
-        <p>E-mail</p>
-        <input type="text"></input>
-        <p>Password</p>
-        <input type="text"></input>
+      <div className="authPageContainer">
+        
+        <div className="authForm">
+          <div className="authHead">Welcome to Nifty network!</div>
+          <div className="authField">
+            <label htmlFor="email">E-mail:</label>
+            <input type="text" id="email"></input>
+          </div>
+          <div className="authField">
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password"></input>
+          </div>
+          <div className="authBtns">
+            <button className="authBtn signInBtn">Sign in</button>
+            <span>or</span>
+            <button className="authBtn signUpBtn">Sign up</button>
+          </div>
+         
+          <div className="authFooter">
+            <div className="authFooterItem"><a href="#" className="authFooterLink">Forgot password?</a></div>
+          </div>
+       </div>
       </div>
-    )
+    );
   }
 }
