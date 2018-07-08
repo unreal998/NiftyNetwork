22338abbeb1313f8fd3e74b48ed81ecddb4f2f33
unreal <unreal9981@gmail.com>
2018-07-08 22:50:26 +0300

    MessagesWindowAndNotificationFixed

--- src/components/MessagesWindow/MessagesWindow.js
+++ src/components/MessagesWindow/MessagesWindow.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-
+import Siders from '../Sider/Sider';
+import Header from '../Header/Header';
 import UsersAside from '../../containers/UsersAside';
 import ConversationsWindow from '../ConversationsWindow/ConversationsWindow';
 import './MessagesWindow.scss';
 
 const MessagesWindow = () => (
-  <div className="messagesWindow">
-    <UsersAside/>
-    <ConversationsWindow/>
-  </div>
+  <Fragment>
+    <Siders/>
+      <div className= "contentPage">
+        <Header/>
+          <div className="messagesWindow">
+            <UsersAside/>
+            <ConversationsWindow/>
+          </div>
+        </div>
+      <Siders/>
+    </Fragment>
 );
 
 MessagesWindow.propTypes = {
